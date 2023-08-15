2553167a8a8bbdae77aaeaa5f128f57d3ba5947b
chiranjib <chiranjibnath21313@gmail.com>
2023-08-16 01:11:32 +0530

    sidemenu and rating added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,14 @@ import { AppName } from "../../utils/constants";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import NavItem from "../NavItem/NavItem";
 import navItems from "../../utils/navItems";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { useLogoutMutation } from "../../app/usersApiSlice";
 import { deleteCredentials } from "../../app/authSlice";
 import { toast } from "react-toastify";
+import SideMenu from "../SideMenu/SideMenu";
 
-const Header = () => {
+const Header = ({ sideMenuOpen, setSideMenuOpen }) => {
   const [activeItem, setActiveItem] = useState("");
   const [search, setSearch] = useState("");
   const { pathname } = useLocation();
@@ -56,53 +57,71 @@ const Header = () => {
     setActiveItem(pathname);
   }, [pathname]);
   return (
-    <header className={`header ${pathname === "/" ? "home" : "other"}`}>
-      <nav>
-        <Link to="/">
-          <h1 className="logo">{AppName}</h1>
-        </Link>
-        <div className="nav-links">
-          <ul>
-            {items.map(({ name, path }) => (
-              <NavItem
-                key={path}
-                name={name}
-                path={path}
-                activeItem={activeItem}
-                setActiveItem={setActiveItem}
-              />
-            ))}
-            {userInfo && (
-              <li title="Logout" className="logout-btn" onClick={logoutHandler}>
-                Logout
-              </li>
-            )}
-          </ul>
-        </div>
-      </nav>
-      {pathname === "/" && (
-        <div className="search-section">
-          <h1>{AppName}</h1>
-          <div className="header-comp search-container">
-            <div className="search-text">Search for best food & drinks</div>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="search">
-                  <AiOutlineSearch />
-                </label>
-                <input
-                  id="search"
-                  type="text"
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  placeholder="Search for dish or restaurant ..."
-                />
-              </div>
-            </form>
-          </div>
-        </div>
+    <>
+      {sideMenuOpen && (
+        <SideMenu
+          sideMenuOpen={sideMenuOpen}
+          setSideMenuOpen={setSideMenuOpen}
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+        />
       )}
-    </header>
+      <header className={`header ${pathname === "/" ? "home" : "other"}`}>
+        <nav>
+          <AiOutlineMenu
+            className={`menu-icon ${pathname !== "/" && "black"}`}
+            onClick={() => setSideMenuOpen(true)}
+          />
+          <Link to="/">
+            <h1 className="logo">{AppName}</h1>
+          </Link>
+          <div className="nav-links">
+            <ul>
+              {items.map(({ name, path }) => (
+                <NavItem
+                  key={path}
+                  name={name}
+                  path={path}
+                  activeItem={activeItem}
+                  setActiveItem={setActiveItem}
+                />
+              ))}
+              {userInfo && (
+                <li
+                  title="Logout"
+                  className="logout-btn"
+                  onClick={logoutHandler}
+                >
+                  Logout
+                </li>
+              )}
+            </ul>
+          </div>
+        </nav>
+        {pathname === "/" && (
+          <div className="search-section">
+            <h1>{AppName}</h1>
+            <div className="header-comp search-container">
+              <div className="search-text">Search for best food & drinks</div>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="search">
+                    <AiOutlineSearch />
+                  </label>
+                  <input
+                    id="search"
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search for dish or restaurant ..."
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </header>
+    </>
   );
 };
 
